0266cc7c1cf70b40282e7b39a78f1d4787f9e05c
jojy-ul <24070769@studentmail.ul.ie>
2024-10-14 10:51:30 +0100

    base changed in vite config.
    base changed in vite config.

--- react-pages/vite.config.ts
+++ react-pages/vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolio/',  // Adjust base URL if necessary
+  base: '/React-github/',  // Adjust base URL if necessary
   build: {
     outDir: 'public',  // Output to public folder
   },
