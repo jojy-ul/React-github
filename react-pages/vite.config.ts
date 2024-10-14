056253201cfe58001a603f9f67b08156eb231fd6
jojy-ul <24070769@studentmail.ul.ie>
2024-10-14 10:48:50 +0100

    Github Pages config added.
    Github Pages config added.

--- react-pages/vite.config.ts
+++ react-pages/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
+  base: '/MyPortfolio/',  // Adjust base URL if necessary
+  build: {
+    outDir: 'public',  // Output to public folder
+  },
 })
